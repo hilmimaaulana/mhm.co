7845b7d43cd104a2425755afe6a92d8c6ee83bb9
hilmimaulana <hilmimaulana0666@gmail.com>
2026-05-15 23:31:01 +0700

    fix: auto migrate for sqlite memory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan; // Tambahkan ini
+use Illuminate\Support\Facades\Schema;  // Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // JURUS OTOMATIS MIGRATION UNTUK VERCEL
+        // Jika pakai sqlite memory dan tabel belum ada, jalankan migrate
+        if (config('database.default') == 'sqlite' && !Schema::hasTable('products')) {
+            Artisan::call('migrate --force');
+            
+            // OPSIONAL: Kalau lo punya seeder buat isi data sepatu, tambahin ini:
+            // Artisan::call('db:seed --force');
+        }
     }
 }
