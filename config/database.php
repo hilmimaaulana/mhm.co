6528496f77397bfc378a6e808edd529dd6f041ed
hilmimaulana <hilmimaulana0666@gmail.com>
2026-05-21 10:20:43 +0700

    fix: database config and tenant pooler subapase ID

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     | to use as your default connection for all database work. Of course
     | you may use many connections at once using the Database library.
     |
-    */
+    | */
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
@@ -25,7 +25,6 @@ return [
     | Here are each of the database connections setup for your application.
     | Of course, examples of configuring each database platform that is
     | supported by Laravel is shown below to make development simple.
-    |
     |
     | All database work in Laravel is done through the PHP PDO facilities
     | so make sure you have the driver for your particular database of
@@ -63,9 +62,10 @@ return [
             ]) : [],
         ],
 
+        // --- FIX SOLUSI PAMUNGKAS SUPABASE POOLER VERCEL ---
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => null, // Kita matikan 'url' agar tidak bentrok atau mengabaikan ENV utama lo gais!
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -89,8 +89,6 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
     ],
